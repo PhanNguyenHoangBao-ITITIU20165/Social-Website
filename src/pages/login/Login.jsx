c0d18c0a2de2f66faf8b2652e047bf7a941c85a5
Mr@JEY <91868353+PhanNguyenHoangBao-ITITIU20165@users.noreply.github.com>
2024-12-20 17:50:58 +0700

    create login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,7 +4,21 @@ const Login = () => {
     return (
         <div className="login">
             <div className="card">
-                
+                <div className="left">
+                    <h1>Hello World</h1>
+                    <p>Welcome to my social media website</p>
+                    <span>Don't you have an account</span>
+                    <button>Register</button>
+                </div>
+
+                <div className="right">
+                    <h1>Login</h1>
+                    <form>
+                        <input type="text" placeholder="Username" />
+                        <input type="password" placeholder="Password" />
+                        <button>Login</button>
+                    </form>
+                </div>
             </div>
         </div>
     )
